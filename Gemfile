f1553b0433ca8cab418a96f521efe739f25f5cc5
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-08-06 23:12:26 +0900

    rspecで必要なgemをインストール,勝手に作成されていたtestディレクトリを削除,テストファイルを作成

--- Gemfile
+++ Gemfile
@@ -42,6 +42,8 @@ group :development, :test do
   gem 'pry-rails'
   gem 'better_errors'
   gem 'binding_of_caller'
+  gem 'factory_bot_rails'
+  gem 'spring-commands-rspec'
 end
 
 group :development do
@@ -59,6 +61,9 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
+  gem 'rspec-rails'
+  gem 'database_cleaner'
+  gem 'webdrivers'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,14 @@ module TaskApp
     config.generators do |g|
       g.assets false
       g.helper false
-      g.test_framework false
+      g.test_framework :rspec,
+                   fixtures: true,
+                   view_specs: false,
+                   helper_specs: false,
+                   routing_specs: false,
+                   controller_specs: false,
+                   request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
 
     # Settings in config/environments/* take precedence over those specified here.

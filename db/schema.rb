81870c604a0ba11c9dedf9d07668104aa5201874
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-09-05 02:35:53 +0900

    【Add】taskテーブルに外部キーを設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_04_122936) do
+ActiveRecord::Schema.define(version: 2019_09_04_124901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,9 @@ ActiveRecord::Schema.define(version: 2019_09_04_122936) do
     t.datetime "task_limit", default: "2019-08-18 18:18:46", null: false
     t.string "task_status"
     t.integer "task_priority", default: 0, null: false
+    t.bigint "user_id"
     t.index ["task_name"], name: "index_tasks_on_task_name"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +37,5 @@ ActiveRecord::Schema.define(version: 2019_09_04_122936) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end

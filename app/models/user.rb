744ee53b886dd38a49b765b2ab5fc7e98e65431a
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-09-06 20:15:39 +0900

    【Fix】バリデーションのスペルミスを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   has_secure_password
 
   before_validation {email.downcase!}
-  validates :email, presence: true, uniqueness: true, length: {maximum: 100}
+  validates :email, presence: true, uniqueness: true, length: {maximum: 100},
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :name, presence: true
-  validates :password, presence: true, length: {minimun: 6}
+  validates :password, presence: true, length: {minimum: 6}
 
   has_many :tasks
 end

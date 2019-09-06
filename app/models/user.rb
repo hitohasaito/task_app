66ec2188a80b59d27a435769214b0cc649514813
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-09-06 19:46:56 +0900

    【Fix】バリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   before_validation {email.downcase!}
-  validates :user, :email, uniqueness: true
+  validates :email, uniqueness: true
 
   has_many :tasks
 end

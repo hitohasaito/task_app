5815cc257d6eed0d82c85f46260a27c281f3c101
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-09-06 19:53:43 +0900

    【Add】モデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ApplicationRecord
   has_secure_password
 
   before_validation {email.downcase!}
-  validates :email, uniqueness: true
+  validates :email, presence: true, uniqueness: true, length: {maximum: 100}
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :name, presence: true
+  validates :password, presence: true, length: {minimun: 6}
 
   has_many :tasks
 end

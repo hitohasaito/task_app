bd335d0112908513bf1258ee0900958843c379dc
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-09-07 15:28:06 +0900

    【Add】ログイン画面とログイン機能を実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      redirect_to new_task_path
+      flash[:notice] = "ログインしました"
+    else
+      render "new"
+      flash[:notice] = "ログインしてください"
+    end
+  end
 end

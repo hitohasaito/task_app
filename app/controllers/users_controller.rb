52a4435b2e46bfbee113332cd5c04cbb1c3ce442
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-09-07 20:25:52 +0900

    【Add】ログイン中にユーザー登録画面にアクセスできないようにする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_id, only:[:show]
+  before_action :check_login
 
   def new
     @user = User.new
@@ -25,5 +26,11 @@ class UsersController < ApplicationController
 
   def set_id
     @user = User.find(params[:id])
+  end
+
+  def check_login
+      @current_user.present?
+      redirect_to new_task_path
+      flash[:notice]= "ユーザー登録済みです"
   end
 end

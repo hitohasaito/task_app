73c7c8a7e188a95020e12e34d19be0fd72d93cfd
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-09-07 18:12:07 +0900

    【Improve】ヘルパーのメソッドを省略

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def current_user
-    @current_user || @currenr_user = User.find_by(id: session[:user_id])
+    @current_user ||=User.find_by(id: session[:user_id])
   end
 
   def logged_in?

f77595bd9d7171bade2378b60af7423663cab5a7
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-09-10 22:06:43 +0900

    【Add】before_actionにアクセス制限を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
-  before_action :set_id, only:[:show]
+  before_action :access_permit,only:[:show]
   before_action :check_login, only:[:new]
+  before_action :set_id, only:[:show]
+  before_action :authenticate_user, only:[:show]
 
   def new
     @user = User.new
@@ -30,7 +32,14 @@ class UsersController < ApplicationController
 
   def check_login
     if current_user.present?
-     redirect_to new_task_path flash[:notice]= "ユーザー登録済みです"
+      redirect_to new_task_path flash[:notice]= "ユーザー登録済みです"
+    end
+  end
+
+  def authenticate_user
+    @user = User.find(params[:id])
+    unless @user.id == current_user.id
+      redirect_to tasks_path flash[:notice]= "権限がありません"
     end
   end
 end

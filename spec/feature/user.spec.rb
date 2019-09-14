1925f6feb6e750c78107e23cbb5a52b52466ab7f
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-09-14 20:46:20 +0900

    【Improve】Factorybotの記述をbackgroundに移動

--- spec/feature/user.spec.rb
+++ spec/feature/user.spec.rb
@@ -1,25 +1,27 @@
 require 'rails_helper'
 
 RSpec.feature "ユーザー", type: :feature do
+  background do
+      FactoryBot.create(:user)
+  end
 
   scenario "ユーザー登録できるとユーザー詳細ページに変移する" do
 
     visit new_user_path
 
-    fill_in "user[name]", with: "name1"
-    fill_in "user[email]", with: "name1@email.com"
-    fill_in "user[password]", with: "password1"
-    fill_in "user[password_confirmation]", with: "password1"
+    fill_in "user[name]", with: "name4"
+    fill_in "user[email]", with: "name4@email.com"
+    fill_in "user[password]", with: "password4"
+    fill_in "user[password_confirmation]", with: "password4"
 
     click_button "登録する"
 
-    expect(page).to have_content "name1"
+    expect(page).to have_content "name4"
     #save_and_open_page
   end
 
   scenario "メールアドレスとパスワードの組み合わせが合っていればログインできること" do
-    FactoryBot.create(:user)
-    
+
     visit new_session_path
 
     fill_in "session[email]", with: "name1@email.com"

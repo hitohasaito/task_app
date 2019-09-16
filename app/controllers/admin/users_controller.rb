7022562064c6965b0826d6ffaf29e77e9a5cf329
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-09-16 14:53:49 +0900

    【Add】ユーザー登録画面に管理者権限のチェックボタンを追加、コントローラーを修正、モデルの日本語化機能を追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,10 +5,18 @@ class Admin::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+
     if @user.save
       redirect_to admin_user_path(@user.id), notice: "ユーザーを登録しました"
     else
       render "new"
     end
+    
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
   end
 end

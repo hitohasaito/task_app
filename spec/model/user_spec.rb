5833e46f900da085bb64dafa0d8fde7ff4d4b7b9
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-09-16 21:57:41 +0900

    【Fix】テストのエラーを修正

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   it "name,email,passwordが空ならバリデーションが通らない" do
-    user = User.new( name: "", email: "", :password: "" )
+    user = User.new(name: '', email: '', password: '')
     expect(user).not_to be_valid
   end
 end

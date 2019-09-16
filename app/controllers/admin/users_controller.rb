6f6f897bca441fb960efd96b5fbde3f1b92fdea0
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-09-16 15:23:56 +0900

    【Add】ユーザー詳細機能を作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,5 @@
 class Admin::UsersController < ApplicationController
+  before_action :set_id, only:[:show]
   def new
     @user = User.new
   end
@@ -7,16 +8,23 @@ class Admin::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to admin_user_path(@user.id), notice: "ユーザーを登録しました"
+      redirect_to admin_user_path(@user.id), notice: "管理者画面でユーザーを登録しました"
     else
       render "new"
     end
-    
+
+  def show
+  end
+
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+  end
+
+  def set_id
+    @user = User.find(params[:id])
   end
 end

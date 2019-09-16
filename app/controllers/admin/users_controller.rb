d96d3fcf7ac0dd1df8e46d20b59027e90b29e8c7
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-09-16 13:56:18 +0900

    【Add】管理者用のユーザー登録機能を追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,14 @@
 class Admin::UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
-  def edit
-  end
-
-  def index
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_user_path(@user.id), notice: "ユーザーを登録しました"
+    else
+      render "new"
+    end
   end
 end

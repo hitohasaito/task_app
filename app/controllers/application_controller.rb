9c1d0f60a9ce32ea8ceab08d4943e119af05b1cf
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-09-21 18:23:47 +0900

    【Add】管理者以外がエラー画面にアクセスしようとした時、専用のエラー画面を表示する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  class MyError < StandardError
+  end
+
+  rescue_from MyError, with: :myerror
+
+  def myerror(e)
+    @error = e
+    render "errors/forbidden"
+  end
 end
